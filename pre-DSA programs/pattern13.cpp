a813bee61cd1ca38b52ddd9e8427e2646f6d9403
Shaurja2005 <saurydiptabhattacharya@gmail.com>
2026-06-19 05:58:38 +0530

    pattern 13 committed

---
+++ pre-DSA programs/pattern13.cpp
@@ -0,0 +1,32 @@
+/* Pattern ->
+
+Enter n: 5
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+*/
+#include <iostream>
+using namespace std;
+class Solution{
+    public:
+        void pattern13(int n){
+            int count =1;
+                for(int i=n ; i>0 ; i--){
+                    for(int j = 1; j <= n-i+1 ; j++){
+                        cout << count << " ";
+                        count++;
+                    }
+                    cout << endl;
+                }
+            }
+};
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    Solution s;
+    s.pattern13(n);
+    return 0;
+}
