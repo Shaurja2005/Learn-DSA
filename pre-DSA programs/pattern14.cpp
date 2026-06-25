66d78fe79c6743269299cb9f3b5e98b2a6ee7720
Shaurja2005 <saurydiptabhattacharya@gmail.com>
2026-06-26 00:15:38 +0530

    pattern 14 commited

---
+++ pre-DSA programs/pattern14.cpp
@@ -0,0 +1,27 @@
+/* Pattern:-
+A
+AB
+ABC
+ABCD
+ABCDE
+for value of n=5*/
+#include <iostream>
+using namespace std;
+class Solution{
+    public:
+    void pattern14(int n){
+        for(int i=0; i<n;i++){
+            for(char j= 'A'; j<='A'+i;j++){
+                cout << j;
+            }
+            cout << endl;
+        }
+    }
+};
+int main(){
+    int n;
+    cout << "enter a number" << endl;
+    cin >> n;
+    Solution s;
+    s.pattern14(n);
+}
